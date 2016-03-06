7e1820e656fc2b212087671bae0166027f82ce11
Liam Brenner <liam.brenner@gmail.com>
2016-03-07 09:05:13 +1100

    Init commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+===============
+wagtail-linkchecker
+===============
+
+Blurb here
+
+Installing
+==========
+
+Install using pip::
+
+    pip install wagtail-charcount
+
+It works with Wagtail 1.0 and upwards.
+
+Using
+=====
+
+TODO
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Install wagtail-charcount using setuptools
+"""
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+from setuptools import find_packages, setup
+
+setup(
+    name='wagtail-charcount',
+    version='0.1.0',
+    description="blurb here",
+    long_description=readme,
+    author='Liam Brenner',
+    author_email='liam@takeflight.com.au',
+    url='https://github.com/takeflight/wagtail-charcount',
+
+    install_requires=[
+        'wagtail>=1.0',
+    ],
+    zip_safe=False,
+    license='BSD License',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+    package_data={},
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
