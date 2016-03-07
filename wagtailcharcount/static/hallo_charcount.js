6d0f13024268bcdb95a7d31f3b42bd5324a6e7c8
Liam Brenner <liam.brenner@gmail.com>
2016-03-07 13:06:09 +1100

    Added javascript plugin and hooks

---
+++ wagtailcharcount/static/hallo_charcount.js
@@ -0,0 +1,47 @@
+$(function (){
+	var rich_text_area = $('.rich_text_area');
+	var parentElement = rich_text_area.parent().parent().parent().parent();
+	var helpBox = parentElement.find('.object-help');
+	if (helpBox.html()) {
+		console.log('the helpbox exists')
+		helpBox.addClass('charcount');
+		helpBox.css({opacity :1});
+	} else {
+		var helpBoxExists = true
+		var elem = parentElement.append("<div style='opacity:1;' class='object-help help charcount'></div>");
+		$('.charcount').hide();
+	}
+	var whiteSpace = /\s\s+/gm;
+	var wordsRegex = /\s+/gi;
+	var charCountElemText = $('.charcount').text();
+
+	var maxChars = rich_text_area.find('textarea').attr('maxlength');
+	$('.richtext').bind('hallomodified', function(event) {
+		var text = event.currentTarget.innerText;
+		var textNoWhitespace = text.replace(whiteSpace, ' ');
+		var textWordCount = textNoWhitespace.trim().replace(wordsRegex, ' ').split(' ').length;
+		var textCharCount = textNoWhitespace.length - 1;
+		var backendCount = $('.richtext').html().length;
+		if (charCountElemText.length > 1) {
+			$('.charcount').html(
+				charCountElemText + '<br><br>' +
+				"Visible characters: " + textCharCount +
+				"<br>Words: " + textWordCount +
+				"<br>Max Chars: " + maxChars +
+				"<br><span class='backend-count'>Backend Count: " + backendCount + '</span>');
+			if (backendCount > maxChars) {
+				$('.backend-count').css({color: 'red'})
+			}
+		} else {
+			$('.charcount').show();
+			$('.charcount').html(
+				"Visible characters: " + textCharCount +
+				"<br>Words: " + textWordCount +
+				"<br>Max Chars: " + maxChars +
+				"<br><span class='backend-count'>Backend Count: " + backendCount + '</span>');
+			if (backendCount > maxChars) {
+				$('.backend-count').css({color: 'red'})
+			}
+		}
+	});
+});
---
+++ wagtailcharcount/wagtail_hooks.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.utils.html import format_html, format_html_join
+from wagtail.wagtailcore import hooks
+
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    js_files = ['hallo_charcount.js']
+    js_includes = format_html_join(
+        '\n',
+        '<script src="{0}{1}"></script>',
+        ((settings.STATIC_URL, filename) for filename in js_files))
+
+    return js_includes + format_html("""
+        <script></script>""")
