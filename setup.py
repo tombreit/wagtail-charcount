2f0268f1abe81da1b9d40e1357c1e6f7b9fc2cf3
Liam Brenner <liam.brenner@gmail.com>
2017-07-03 10:57:11 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtail-charcount',
-    version='0.1.1',
+    version='0.1.2',
     description="A wagtail character and word counting plugin for RichTextFields.",
     long_description=readme,
     author='Liam Brenner',
